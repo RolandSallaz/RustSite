18eae39716e2d054c2b233e97e9e0c1a2481f38b
unknown <machodin@yandex.ru>
2022-12-28 17:54:18 +0300

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../images/logo.png'
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <h1 className="header__text">Rust server</h1>
+        <p className='header__auth-state'>Не авторизован</p>
+      </header>
+      <main className="main">
+        <button className='main__login-button'>Войти через Steam</button>
+      </main>
+      <footer className="footer">
+        <p className='footer__copyright'>&#169; KakahaGames 2023</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
