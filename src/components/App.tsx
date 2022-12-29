66a3e846672528b7fd7e0678b0c38db2ffda1769
unknown <machodin@yandex.ru>
2022-12-29 15:51:46 +0300

    visual update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,11 +7,12 @@ function App() {
       <header className="header">
 
         <div className="logo" />
-        <h1 className="header__text">Rust server</h1>
-        <p className="header__auth-state">Не авторизован</p>
+        <h1 className="header__text">KAKAMBE Rust server</h1>
+        {/* auth state */}
+        <p className="header__auth-state"></p>
       </header>
       <main className="main">
-        <button className="main__login-button">Войти через Steam</button>
+        <button className="login-button"><p className='login-button__text'>Войти через Steam </p><div className='loggin-button__logo'/></button>
       </main>
       <footer className="footer">
         <p className="footer__copyright">&#169; KakahaGames 2023</p>
